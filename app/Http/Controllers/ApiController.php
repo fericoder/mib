87f1c240f94b5d95b376c8f6e40c4bfd762a5a76
fericoder <farzane.a.shirazi26@gmail.com>
2020-08-08 19:03:50 +0430

    en 2 fa added

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,14 +11,14 @@ class ApiController extends Controller
         if ($request->token !== 'cAxNzPS33wbX1pjbVEg1'){
             abort(403, 'Unauthorized Action');
         }
-        if (!is_numeric($request->amount)){
+        if (!is_numeric($this->fa_num_to_en($request->amount))){
             abort(403, 'Invalid Amount');
         }
-        if (!\DB::table('specification_item_groups')->where('p_id', $request->id)->first()){
+        if (!\DB::table('specification_item_groups')->where('p_id', $this->fa_num_to_en($request->id))->first()){
             abort(403, 'Invalid Id');
         }
-        \DB::table('specification_item_groups')->where('p_id', $request->id)->update(['amount' => $request->amount]);
-        return response()->json(['Massage' => 'Done!', 'New Amount Is' => $request->amount]);
+        \DB::table('specification_item_groups')->where('p_id', $this->fa_num_to_en($request->id))->update(['amount' => $this->fa_num_to_en($request->amount)]);
+        return response()->json(['Massage' => 'Done!', 'New Amount Is' => $this->fa_num_to_en($request->amount)]);
 
     }
 
@@ -28,13 +28,13 @@ class ApiController extends Controller
         if ($request->token !== 'cAxNzPS33wbX1pjbVEg1'){
             abort(403, 'Unauthorized Action');
         }
-        if (!is_numeric($request->user_id)){
+        if (!is_numeric($request->$this->fa_num_to_en(user_id))){
             abort(403, 'Invalid User');
         }
-        if (!\DB::table('users')->where('crm_id', $request->user_id)->first()){
+        if (!\DB::table('users')->where('crm_id', $this->fa_num_to_en($request->user_id))->first()){
             abort(403, 'Invalid User');
         }
-        $purchases = \DB::table('user_purchases')->where('user_id', \DB::table('users')->where('crm_id', $request->user_id)->first()->id)->get();
+        $purchases = \DB::table('user_purchases')->where('user_id', \DB::table('users')->where('crm_id', $this->fa_num_to_en($request->user_id))->first()->id)->get();
         return response()->json($purchases);
 
     }
