400878d54839276ca870d223e8131c880e21b1a2
fericoder <farzane.a.shirazi26@gmail.com>
2020-08-11 18:48:48 +0430

    purchuses

---
+++ resources/views/dashboard/users/purchases.blade.php
@@ -0,0 +1,245 @@
+@extends('dashboard.layouts.master', ['title' => 'لیست سفارشات مشتری'])
+
+@section('headerScripts')
+    <link href="/assets/plugins/custom/datatables/datatables.bundle.rtl.css" rel="stylesheet" type="text/css"/>
+
+    <style>
+        a, span {
+            font-family: byekan !important;
+        }
+        .dataTables_info{
+            font-family: BYekan;
+        }
+        select{
+            font-family: BYekan;
+        }
+        td{
+            vertical-align: middle!important;
+        }
+
+        td, th{
+            vertical-align: middle!important;
+            font-family: iranyekan;
+        }
+        .select2-container--default .select2-selection--single .select2-selection__arrow, .select2-container--default .select2-selection--multiple .select2-selection__arrow{
+            font-family: "LineAwesome"!important;
+        }
+    </style>
+@stop
+
+
+@section('content')
+
+
+
+    <!-- begin:: Content Head -->
+    <div class="kt-subheader   kt-grid__item" id="kt_subheader">
+        <div class="kt-container ">
+            <div class="kt-subheader__main">
+
+                <h3 class="kt-subheader__title">داشبورد مدیریت </h3>
+
+                <span class="kt-subheader__separator kt-subheader__separator--v"></span>
+
+                <span class="kt-subheader__desc iranyekan">لیست سفارشات مشتری</span>
+            </div>
+
+        </div>
+    </div>
+    <!-- end:: Content Head -->
+    <!-- begin:: Content -->
+    <div class="kt-container  kt-grid__item kt-grid__item--fluid">
+        <div class="row">
+            <div class="col-xl-12">
+
+
+
+
+
+                <div class="kt-portlet kt-portlet--mobile">
+                    <div class="kt-portlet__head">
+                        <div class="kt-portlet__head-label">
+                            <h3 class="kt-portlet__head-title">
+                                لیست سفارشات مشتری
+                            </h3>
+                        </div>
+
+                        <div style="" class="kt-portlet__head-toolbar">
+                            <div class="kt-portlet__head-toolbar-wrapper">
+                                <div class="dropdown dropdown-inline">
+                                    <button style="" type="button" class="btn btn-brand btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="la la-plus"></i> ابزار ها و خروجی ها
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-right">
+                                        <ul class="kt-nav">
+                                            <li class="kt-nav__section kt-nav__section--first">
+                                                <span class="kt-nav__section-text">انواع خروجی ها</span>
+                                            </li>
+                                            <li class="kt-nav__item">
+                                                <a href="#" class="kt-nav__link" id="export_print">
+                                                    <i class="kt-nav__link-icon la la-print"></i>
+                                                    <span class="kt-nav__link-text">چاپ</span>
+                                                </a>
+                                            </li>
+                                            <li class="kt-nav__item">
+                                                <a href="#" class="kt-nav__link" id="export_copy">
+                                                    <i class="kt-nav__link-icon la la-copy"></i>
+                                                    <span class="kt-nav__link-text">کپی</span>
+                                                </a>
+                                            </li>
+                                            <li class="kt-nav__item">
+                                                <a href="#" class="kt-nav__link" id="export_excel">
+                                                    <i class="kt-nav__link-icon la la-file-excel-o"></i>
+                                                    <span class="kt-nav__link-text">اکسل</span>
+                                                </a>
+                                            </li>
+                                            <li class="kt-nav__item">
+                                                <a href="#" class="kt-nav__link" id="export_csv">
+                                                    <i class="kt-nav__link-icon la la-file-text-o"></i>
+                                                    <span class="kt-nav__link-text">CSV</span>
+                                                </a>
+                                            </li>
+                                            <li class="kt-nav__item">
+                                                <a href="#" class="kt-nav__link" id="export_pdf">
+                                                    <i class="kt-nav__link-icon la la-file-pdf-o"></i>
+                                                    <span class="kt-nav__link-text">PDF</span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="kt-portlet__body">
+                        <!--begin: Datatable -->
+                        <table style="font-family: iranyekan; width: 100%" class="table table-striped table-bordered table-hover table-checkable display nowrap" id="m_table_2">
+                            <thead style="font-family: BYekan">
+                            <tr>
+                                <th title="Field #1" data-field="1">شناسه سفارش</th>
+                                <th title="Field #1" data-field="1">تاریخ ثبت</th>
+                                <th title="Field #1" data-field="1">مجموع قیمت</th>
+                                <th title="Field #2" data-field="2">آدرس</th>
+                                <th title="Field #5" data-field="5">روش پرداخت</th>
+                                <th title="Field #5" data-field="5">وضعیت</th>
+                                <th title="Field #6" data-field="6">لیست کالاها</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            @foreach ($purchases as $purchase)
+                                <tr>
+                                    <td style="font-family: BYekan" >{{ $purchase->id }}</td>
+                                    <td style="font-family: BYekan!important; direction: ltr" >{{ jdate($purchase->created_at) }}</td>
+                                    <td style="font-family: BYekan" >{{ number_format($purchase->total_price) }}</td>
+                                    <td style="font-family: BYekan" >{{ $purchase->address->province . ' - ' . $purchase->address->city . ' - ' . $purchase->address->address . ' - ' . $purchase->address->tel . ' - ' . $purchase->address->fullName . ' - ' . $purchase->address->zip_code  }}</td>
+                                    <td style="font-family: BYekan" >{{ $purchase->payment_method }}</td>
+                                    <td style="font-family: BYekan" >{{ $purchase->status }}</td>
+                                    <td>
+                                        <a style="margin: 5px;" href="{{ route('users.purchase.show', ['userID' => $purchase->user->id, 'id' => $purchase->id] ) }}"  title="ویرایش" ><i class="far fa-eye text-info mr-1 button font-15"></i></a>
+                                    </td>
+
+                                </tr>
+                            @endforeach
+
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+    <!-- end:: Content -->
+
+@stop
+
+
+@section('footerScripts')
+    <script src="/assets/plugins/custom/datatables/datatables.bundle.js" type="text/javascript"></script>
+
+
+    <!--begin::Page Scripts -->
+    <script>
+
+
+
+        $(document).on('click', '#remove', function (e) {
+            e.preventDefault();
+            var id = $(this).data('id');
+            swal({
+                    title: "آیا مطمئن هستید؟",
+                    type: "error",
+                    confirmButtonClass: "btn-danger",
+                    confirmButtonText: "بله",
+                    cancelButtonText: "خیر",
+                    showCancelButton: true,
+                },
+                function () {
+                    $.ajax({
+                        type: "POST",
+                        url: "{{url('/dashboard/users/delete')}}",
+                        data: {id: id},
+                        success: function (data) {
+                            var url = document.location.origin + "/dashboard/users";
+                            location.href = url;
+                        }
+                    });
+                });
+        });
+
+
+
+        var DatatablesExtensionButtons = {
+            init: function () {
+                var t;
+                t = $("#m_table_2").DataTable({
+                        "searching": true,
+                        scrollY:"50vh",scrollX:!0,scrollCollapse:!0,
+                        responsive: !0,
+
+                        buttons: ["print", "copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+                    }
+                ),
+                    $("#export_print").on("click", function (e) {
+                            e.preventDefault(), t.button(0).trigger()
+                        }
+                    ),
+                    $("#export_copy").on("click", function (e) {
+                            e.preventDefault(), t.button(1).trigger()
+                        }
+                    ),
+                    $("#export_excel").on("click", function (e) {
+                            e.preventDefault(), t.button(2).trigger()
+                        }
+                    ),
+                    $("#export_csv").on("click", function (e) {
+                            e.preventDefault(), t.button(3).trigger()
+                        }
+                    ),
+                    $("#export_pdf").on("click", function (e) {
+                            e.preventDefault(), t.button(4).trigger()
+                        }
+                    )
+
+
+
+            }
+        };
+        jQuery(document).ready(function () {
+                DatatablesExtensionButtons.init()
+            }
+        );
+
+        $("#show").click(function () {
+            $("#edu").toggle('slow');
+        });
+
+    </script>
+    <!--end::Page Scripts -->
+    <script src="/assets/js/pages/crud/forms/widgets/select2.js" type="text/javascript"></script>
+
+@stop
