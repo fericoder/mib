c607156426e9676b3c0d64726f72205d65ed644a
fericoder <farzane.a.shirazi26@gmail.com>
2020-08-27 17:36:13 +0430

    front-end template is added

--- resources/views/dashboard/users/edit.blade.php
+++ resources/views/dashboard/users/edit.blade.php
@@ -172,6 +172,7 @@
 
                                         </div>
 
+
                                         <!--end card-body-->
                                     </div>
                                     <!--end card-->
